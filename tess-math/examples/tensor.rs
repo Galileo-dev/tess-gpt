a9c766bb2c4a3b8ae189e09d720fb43e8a779ce8
galileo-dev <fionn2200@gmail.com>
2023-04-11 11:55:52 +0100

    stacking tensors

--- tess-math/examples/tensor.rs
+++ tess-math/examples/tensor.rs
@@ -1,18 +1,21 @@
 use tess_math::math::Tensor;
 
 fn main() {
+    println!("========================");
+    println!("=        Tensor        =");
+    println!("========================");
+
     let tensor = Tensor::new(vec![1, 2, 3, 4], vec![2, 2]);
     println!("{:?}", tensor);
 
-    println!("{:?}", tensor.get(0, 1));
+    // 3x3 tensor
+    let tensor = Tensor::new(vec![1, 2, 3, 4, 5, 6, 7, 8, 9], vec![3, 3]);
+    println!("{:?}", tensor);
 
-    println!("{:?}", tensor.get(1, ..2));
-
-    println!("{:?}", tensor.get(.., 1));
-
-    println!("{:?}", tensor.get(.., ..2));
-
-    println!("{:?}", tensor.get(.., ..));
-
-    println!("{:?}", tensor.get(.., 1..));
+    // stack 3 tensors
+    let tensor1 = Tensor::new(vec![1, 2, 3, 4], vec![2, 2]);
+    let tensor2 = Tensor::new(vec![5, 6, 7, 8], vec![2, 2]);
+    let tensor3 = Tensor::new(vec![9, 10, 11, 12], vec![2, 2]);
+    let tensor = Tensor::stack(&vec![tensor1, tensor2, tensor3], 0);
+    println!("{:?}", tensor);
 }
--- tess-math/src/math/tensor.rs
+++ tess-math/src/math/tensor.rs
@@ -1,7 +1,9 @@
 use rand::prelude::*;
 use std::{
-    fmt::Debug,
+    fmt::{Debug, Formatter},
+    mem,
     ops::{Range, RangeFrom, RangeFull, RangeInclusive, RangeTo, RangeToInclusive},
+    slice,
 };
 
 // ===========================================================
@@ -10,7 +12,7 @@ use std::{
 // Date: 2023-04-09
 // ===========================================================
 
-#[derive(Debug, PartialEq)]
+#[derive(PartialEq)]
 pub struct Tensor<T> {
     shape: Vec<usize>,
     data: Vec<T>,
@@ -186,6 +188,102 @@ where
         Tensor::new(data, vec![row_indices.len(), column_indices.len()])
     }
 
+    fn num_elements(&self) -> usize {
+        self.shape.iter().product()
+    }
+
+    fn transpose(&mut self, axis1: usize, axis2: usize) {
+        let n = self.shape.len();
+        assert!(axis1 < n && axis2 < n);
+
+        let (a, b) = if axis1 < axis2 {
+            (axis1, axis2)
+        } else {
+            (axis2, axis1)
+        };
+
+        let mut indices = (0..n).collect::<Vec<_>>();
+        indices.swap(a, b);
+
+        let mut new_data = vec![mem::MaybeUninit::<T>::uninit(); self.num_elements()];
+        let mut strides = vec![1; n];
+        for i in (0..n).rev() {
+            strides[indices[i]] = if i == n - 1 {
+                1
+            } else {
+                strides[indices[i + 1]] * self.shape[indices[i + 1]]
+            };
+        }
+
+        for (i, element) in self.data.iter().enumerate() {
+            let mut index = i;
+            let mut new_index = 0;
+            for j in 0..n {
+                let stride = strides[indices[j]];
+                let dim_size = self.shape[indices[j]];
+                let old_index = index / stride % dim_size;
+                new_index += old_index * stride;
+                index -= old_index * stride;
+            }
+            new_data[new_index] = mem::MaybeUninit::new(element.clone());
+        }
+
+        self.data = unsafe { new_data.into_iter().map(|x| x.assume_init()).collect() };
+        self.shape = indices.into_iter().map(|i| self.shape[i]).collect();
+    }
+
+    /// stacks multiple tensors together given an axis
+    ///
+    /// # Examples
+    /// ```
+    /// use tess_math::math::Tensor;
+    ///
+    /// let tensor1 = Tensor::new(vec![1, 2, 3, 4, 5, 6], vec![2, 3]);
+    /// let tensor1 = &[1.0, 2.0, 3.0];
+    /// let tensor2 = &[4.0, 5.0, 6.0, 7.0];
+    /// let tensor3 = &[8.0];
+    ///
+    /// let stacked = stack_tensors(&[tensor1, tensor2, tensor3], 0);
+    /// assert_eq!(stacked, vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0]);
+    pub fn stack(tensors: &[Tensor<T>], axis: usize) -> Tensor<T> {
+        let mut new_shape = tensors[0].shape.clone();
+        new_shape[axis] = tensors.iter().map(|t| t.shape[axis]).sum();
+
+        let mut new_data = Vec::with_capacity(new_shape.iter().product());
+        let mut strides = vec![1; new_shape.len()];
+        for i in (0..new_shape.len()).rev() {
+            strides[i] = if i == new_shape.len() - 1 {
+                1
+            } else {
+                strides[i + 1] * new_shape[i + 1]
+            };
+        }
+
+        let mut offset = 0;
+        for tensor in tensors {
+            assert_eq!(tensor.shape.len(), new_shape.len());
+            for (i, dim_size) in tensor.shape.iter().enumerate() {
+                if i != axis {
+                    assert_eq!(dim_size, &new_shape[i]);
+                }
+            }
+            for (i, element) in tensor.data.iter().enumerate() {
+                let mut index = i;
+                let mut new_index = offset;
+                for j in 0..new_shape.len() {
+                    let stride = strides[j];
+                    let dim_size = new_shape[j];
+                    let old_index = index / stride % dim_size;
+                    new_index += old_index * stride;
+                    index -= old_index * stride;
+                }
+                new_data.push(element.clone());
+            }
+            offset += tensor.shape[axis];
+        }
+        Tensor::new(new_data, new_shape)
+    }
+
     fn index(&self, row: usize, col: usize) -> T {
         self.data[row * self.shape[1] + col]
     }
@@ -267,6 +365,50 @@ where
     }
 }
 
+// impl Debug
+impl<T> Debug for Tensor<T>
+where
+    T: Copy + Debug + PartialEq<i32>,
+{
+    // add new line after each row in the tensor
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        // pytorch style
+        let mut s = String::new();
+
+        let indent1 = " ".repeat(8);
+        let indent2 = " ".repeat(15);
+
+        s.push_str("Tensor{ shape: ");
+        s.push_str(&format!("{:?}", self.shape));
+        s.push_str(",\n");
+        s.push_str(&indent1);
+        s.push_str("data:\n");
+
+        // add to the start of each row from this point on
+
+        let mut index = 0;
+        for i in 0..self.shape[0] {
+            s.push_str(&indent2);
+            s.push_str("[");
+            for j in 0..self.shape[1] {
+                s.push_str(&format!("{:?}", self.index(i, j)));
+                if j != self.shape[1] - 1 {
+                    s.push_str(", ");
+                }
+            }
+            s.push_str("]");
+            s.push_str(",");
+            if i != self.shape[0] - 1 {
+                s.push_str("\n");
+            }
+        }
+        s.push_str("\n}");
+        write!(f, "{}", s);
+
+        Ok(())
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -314,5 +456,24 @@ mod tests {
         assert_eq!(tensor.get(1..2, 0..3).data(), &[4, 5, 6]);
         assert_eq!(tensor.get(0..2, 2..=2).data(), &[3, 6]);
         assert_eq!(tensor.get(1..2, &[0, 1, 2]).data(), &[4, 5, 6]);
+    }
+
+    #[test]
+    fn test_stack() {
+        let tensor1 = Tensor::new(vec![1, 2, 3, 4], vec![2, 2]);
+        let tensor2 = Tensor::new(vec![5, 6, 7, 8], vec![2, 2]);
+        let tensor3 = Tensor::new(vec![9, 10, 11, 12], vec![2, 2]);
+        let tensor = Tensor::stack(&vec![tensor1, tensor2, tensor3], 0);
+
+        assert_eq!(tensor.data(), &[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]);
+        assert_eq!(tensor.shape(), &[6, 2]);
+
+        let tensor1 = Tensor::new(vec![1, 2, 3, 4], vec![2, 2]);
+        let tensor2 = Tensor::new(vec![5, 6, 7, 8], vec![2, 2]);
+        let tensor3 = Tensor::new(vec![9, 10, 11, 12], vec![2, 2]);
+        let tensor = Tensor::stack(&vec![tensor1, tensor2, tensor3], 1);
+
+        assert_eq!(tensor.data(), &[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]);
+        assert_eq!(tensor.shape(), &[2, 6]);
     }
 }
